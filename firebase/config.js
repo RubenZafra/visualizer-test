5ba8c81d66e9c4b8f6c7580bfe846cb5e177e7b0
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-05-23 12:41:21 +0200

    Feat: Loaded Points and Materials from db

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,4 @@
-const config = {
+export const config = {
   apiKey: 'AIzaSyC_JwpXS4uj9sRRDrbFAtalE1QulNTmKnw',
   authDomain: 'visualizer-new-devs-test.firebaseapp.com',
   projectId: 'visualizer-new-devs-test',
@@ -6,5 +6,3 @@ const config = {
   messagingSenderId: '702664185241',
   appId: '1:702664185241:web:580752c50d570d0c89ef08'
 }
-
-export default config
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,60 @@
+import { config } from '../firebase/config'
+import  { initializeApp } from 'firebase/app'
+import {
+  getFirestore, 
+  collection, 
+  getDocs
+} from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+
+// connect to DB
+initializeApp(config)
+const db = getFirestore()
+
+// reference to collections
+const pointsRef = collection(db, 'points')
+const materialsRef = collection(db, 'materials')
+
+
 export default function Home () {
+
+  const [points, setPoints] = useState([])
+  const [materials, setMaterials] = useState([])
+
+  useEffect(() => {
+    ( async () => {
+      const points = await getDocs(pointsRef)
+      const pointsData = points.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      })
+      setPoints(pointsData)
+    }
+    )()
+  }, [])
+
+  useEffect(() => {
+    ( async () => {
+      const materials = await getDocs(materialsRef)
+      const materialsData = materials.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      })
+      setMaterials(materialsData)
+    }
+    )()
+  }, [])
+
+
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      ESTUDIO CACTUS VISUALIZER TEST
-    </div>
+   
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        ESTUDIO CACTUS VISUALIZER TEST
+      </div>
+    
   )
 }
